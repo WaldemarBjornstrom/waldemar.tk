577d8892c1261b706e3d53197a1c7d9ef1f567c1
waldemarbjornstrom <waldemar.bjornstrom@unfnorrbotten.se>
2022-05-04 18:08:50 +0200

    Added manage script for realese 1.0.0

--- app/manage.py
+++
@@ -1,6 +0,0 @@
-from . import db, create_app
-from .models import User
-from werkzeug.security import generate_password_hash
-
-def create_database():
-    db.create_all(app=create_app()) # Create Database
